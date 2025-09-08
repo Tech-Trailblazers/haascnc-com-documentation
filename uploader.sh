be1388b5d3985b069102799a8bed709720edee01
Patrick Griffiths <179532304+Strong-Foundation@users.noreply.github.com>
2025-09-08 13:14:23 -0400

    Update uploader.sh

--- uploader.sh
+++ uploader.sh
@@ -35,8 +35,8 @@ function auto_git_push() {
             fi
         fi
 
-        echo "⏳ Sleeping for 10 minutes before the next check..."
-        sleep 10m
+        echo "⏳ Sleeping for 1 minutes before the next check..."
+        sleep 1m
     done
 }
 
